d451d6d61cd28a640b43f8f963c041cc04bfdc8f
Your <cabatol@csumb.edu>
2017-02-03 04:47:26 +0000

    please god let this work

--- proj1.py
+++ proj1.py
@@ -1,4 +1,20 @@
+import os, numpy, PIL
 from PIL import Image
 
-img = Image.open("1.png")
-img.show()
+files = os.listdir(os.getcwd())
+imgs = [filename for filename in files if filename[-4:] in [".png"]]
+
+width, height = Image.open(imgs[0]).size
+n = len(imgs)
+
+arr = numpy.zeros((height, width, 3), numpy.float)
+
+for i in imgs:
+    imgarr = numpy.array(Image.open(i), dtype=numpy.float)
+    arr = arr+imgarr/n
+    
+arr = numpy.array(numpy.round(arr), dtype = numpy.uint8)
+
+finalImg = Image.fromarray(arr, mode = "RGB")
+finalImg.save("finalImg.png")
+finalImg.show()
